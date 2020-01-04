dce480c2db7de99305f5e01da85b307f22f1bc2e
Shelwin Xiao <562687381@qq.com>
2020-01-04 11:50:05 +0000

    meet error

--- qlearning_exploration/train.py
+++ qlearning_exploration/train.py
@@ -4,6 +4,9 @@ import re
 import random
 import numpy as np
 
+from numpy import savetxt
+from numpy import loadtxt
+
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -35,6 +38,7 @@ def get_hyperlinks(url):
         links.remove("/2018/01/01/demo/")
     return links
 
+'''
 gamma = 0.8
 
 q_matrix = np.full((64,64), 10000)
@@ -42,13 +46,19 @@ q_matrix = np.full((64,64), 10000)
 current_url = "https://slxiao.github.io"
 current_state = hash(current_url) % 64
 
-click_number = 0
+stopping_iterations = 0
 
 state_action_record = np.zeros((64,64))
 
-while click_number < 10:
+#links_mapping = {}
+
+#links_mapping[current_state] = current_url
+
+while stopping_iterations < 1000:
     available_links = get_hyperlinks(current_url)
     available_actions = [hash(link) % 64 for link in available_links]
+
+    #links_mapping.update({available_actions[i]: available_links[i] for i in range(len(available_links))})
 
     max_q_value = -1
     max_q_action = None
@@ -63,6 +73,8 @@ while click_number < 10:
     next_state = hash(next_url) % 64
     next_available_links = get_hyperlinks(next_url)
     next_available_actions = [hash(link) % 64 for link in next_available_links]
+
+    #links_mapping.update({next_available_actions[i]: next_available_links[i] for i in range(len(next_available_links))})
     
     if state_action_record[current_state][max_q_action] == 0:
         reward = 10000
@@ -82,9 +94,9 @@ while click_number < 10:
 
     q_matrix[current_state][max_q_action] = reward + gamma * max_future_q_value
 
-    click_number += 1
+    stopping_iterations += 1
 
-    if click_number > 0 and not click_number % 100:
+    if stopping_iterations > 0 and not stopping_iterations % 100:
         current_url = "https://slxiao.github.io"
         current_state = hash(current_url) % 64
     else:
@@ -93,5 +105,46 @@ while click_number < 10:
 
 
 print(q_matrix)
+
+savetxt('data.csv', q_matrix, delimiter=',')
+'''
+q_matrix = loadtxt('data.csv', delimiter=',')
+
+
+click_number = 0
+unique_links = []
+points = [(click_number, len(unique_links))]
+
+current_url = "https://slxiao.github.io"
+
+while click_number < 1000:
+    print(current_url)
+    links = get_hyperlinks(current_url)
+    current_state = hash(current_url) % 64
+
+    q_value_list = []
+    for link in links:
+        q_value_list.append((link, q_matrix[current_state][hash(link) % 64]))
+
+    max_q_value = max([i[1] for i in q_value_list])
+
+    candidate_links = [i[0] for i in q_value_list if i[1] == max_q_value]
+
+    print(q_value_list)
+    next_link = random.choice(candidate_links)
+    
+    if next_link not in unique_links:
+        unique_links.append(next_link)
+    
+    points.append((click_number, len(unique_links)))
+    click_number += 1
+
+    print(click_number, len(unique_links))
+
+    if not click_number % 100:
+        current_url = "https://slxiao.github.io"
+    else:
+        current_url = next_link
+
 
     
